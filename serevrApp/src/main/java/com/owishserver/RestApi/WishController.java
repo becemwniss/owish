e95df644ad9547a5916d7bf5f51e8574bba5af04
becem <becemw@gmail.com>
2017-07-19 20:42:54 +0200

    fix resttemplate

--- serevrApp/src/main/java/com/owishserver/RestApi/WishController.java
+++ serevrApp/src/main/java/com/owishserver/RestApi/WishController.java
@@ -1,22 +1,24 @@
 package com.owishserver.RestApi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.owishserver.entities.Wish;
 import com.owishserver.models.WishModel;
 import com.owishserver.service.WishService;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
+import java.net.InetAddress;
 
 /**
  * Created by pc-becem on 6/25/2017.
@@ -39,10 +41,13 @@ public class WishController {
     @RequestMapping(value = "/findAllWishs", method = RequestMethod.GET)
     public ResponseEntity<?> findAllWishs(){
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            Object o = restTemplate.getForObject("http://localhost:9200/wish-index/data/_search", Object.class);
-            return new ResponseEntity<>(o, HttpStatus.OK);
-        } catch (RestClientException e) {
+            TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+
+            SearchResponse response = client.prepareSearch("wish-index")
+                    .setTypes("data").addSort("creation_date", SortOrder.DESC).execute().actionGet();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
